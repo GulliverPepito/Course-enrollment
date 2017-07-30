4e7aee6aaf098c3c56415008094623142c5cd6e5
Antonio Ossa <aaossa@uc.cl>
2017-07-29 22:22:14 -0400

    Enable signing up/in via GitHub
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < ApplicationController
 
 	def create
-		render :plain => request.env['omniauth.auth']
+		auth = request.env['omniauth.auth']
+		unless @auth = Identity.find_from_hash(auth)
+			# Create a new user or add an auth to existing user, depending on
+			# whether there ir already a user signed in
+			@auth = Identity.create_from_hash(auth, current_user)
+		end
+		# Log the authorizing user in
+		self.current_user = @auth.user
+
+		render :plain => "Hi #{current_user.name} (#{current_user.nickname})"
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
 	has_many :identities
+
+	def self.create_from_hash!(hash)
+		logger.debug hash.to_yaml
+		create(
+			:nickname => hash['info']['nickname'],
+			:name => hash['info']['name'],
+			:email => hash['info']['email'],
+			:image_url => hash['info']['image'],
+			:token => hash['credentials']['token']
+		)
+	end
 end
