ddccf94f9e6db2ef9cd4a3a931af4596ff0cc5b4
Antonio Ossa <aaossa@uc.cl>
2017-07-30 14:22:36 -0400

    Handle authentication failure
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 	root 'pages#home'
 	match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 	match '/signout', to: 'sessions#destroy', via: [:get, :post]
+	match '/auth/failure', to: 'sessions#failure', via: [:get, :post]
 end
